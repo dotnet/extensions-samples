2a335c1c2429b8dffabb1f1e58a1dbadfa8bc06e
Igor Velikorossov <igor.velikorossov@microsoft.com>
2023-05-09 10:50:05 +1000

    Update packages / rename projects

--- src/Diagnostics/ExceptionSummarization/MyService.cs
+++ src/Diagnostics/ExceptionSummarization/MyService.cs
@@ -5,6 +5,7 @@ using System;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.ExceptionSummarization;
 using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Extensions.ExceptionSummarization.Sample;
