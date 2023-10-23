98b5b44bdf1caa51c685448156a778914eac1b5a
Igor Velikorossov <igor.velikorossov@microsoft.com>
2023-10-23 12:33:35 +1100

    Update to .NET 9

--- src/Telemetry/LatencyMeasurement/LatencyContext/Middleware/ApplicationMiddleware.cs
+++ src/Telemetry/LatencyMeasurement/LatencyContext/Middleware/ApplicationMiddleware.cs
@@ -5,7 +5,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Telemetry.Latency;
+using Microsoft.Extensions.Diagnostics.Latency;
 
 namespace LatencyContext
 {
