e21ccdc9d3d746b09301573d9fea77fdfcce0b4c
Nikita Balabaev <nbalabaev@microsoft.com>
2023-11-09 17:01:27 +0100

    HttpClient logging sample

---
+++ src/Telemetry/Logging/HttpClientLogging/StarRedactor.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Microsoft.Extensions.Compliance.Redaction;
+
+namespace HttpClientLogging;
+
+internal sealed class StarRedactor : Redactor
+{
+    private const char Replacement = '*';
+
+    public override int GetRedactedLength(ReadOnlySpan<char> source)
+        => source.Length;
+
+    public override int Redact(ReadOnlySpan<char> source, Span<char> destination)
+    {
+        var len = source.Length;
+        var redacted = new string(Replacement, len);
+        redacted.AsSpan().CopyTo(destination);
+        return len;
+    }
+}
