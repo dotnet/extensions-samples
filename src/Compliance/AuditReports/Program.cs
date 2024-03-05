cf562c2df9a6ddf8c4979232e213527646fb35b5
Nikita Balabaev <nbalabaev@microsoft.com>
2024-03-05 14:48:25 +0100

    fixes

--- src/Compliance/AuditReports/Program.cs
+++ src/Compliance/AuditReports/Program.cs
@@ -1,14 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-const string ReportLocation = "../../../report/net8.0/";
+const string ReportLocation = "./";
 const string ComplianceReportFileName = "ComplianceReport.json";
-const string MetricsReportFileName = "MetricsReport.json";
 
 Console.WriteLine("Starting AuditReports with arguments: " + string.Join(";", args));
 
 CheckReport(ComplianceReportFileName);
-CheckReport(MetricsReportFileName);
 
 static void CheckReport(string fileName)
 {
